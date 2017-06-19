1f6cafa66a012a515c5a5eef4d23e6c106fe6a4e
Curtis Frazier <fraziercurtis2003@yahoo.com>
2017-06-19 14:41:33 -0400

    initial commit

---
+++ week5day1.js/assessment/main.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+var loremIpsum = require('lorem-ipsum')
+var output = loremIpsum({
+    count: 3
+  , units: 'paragraphs'
+  , sentenceLowerBound: 5
+  , sentenceUpperBound: 15
+  , paragraphLowerBound: 3
+  , paragraphUpperBound: 7
+  , format: 'html'
+});
+
+app.get('/lorem', function (req, res ) {
+  res.send(output);
+});
+
+app.listen(3000,function () {
+  console.log("Hello World");
+})
